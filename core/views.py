5468a037e00d0ffa99fabb237be3bc635c8e2f13
GS-suzuki13 <gustavosuzuki3@gmail.com>
2021-09-08 12:22:15 -0300

    Incluindo tela e funcao de autenticacao, feito acesso pela pagina de login para tela de admin do django

--- core/views.py
+++ core/views.py
@@ -1,17 +1,50 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from core.models import Evento
+
 
 #
 # def index(request):
 #     return redirect('/admin/')
+@csrf_exempt
+def login_user(request):
+    return render(request, 'login.html')
 
+
+@csrf_exempt
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@csrf_exempt
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou Senha Inválidos")
+    return redirect('/')
+
+
+@csrf_exempt
 def local_evento(request, titulo):
     consulta = Evento.objects.get(titulo=titulo)
     return HttpResponse(consulta.local)
 
 
+@csrf_exempt
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
